5ca7b965b0a2e0d2139533737642c628e8b613fe
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-13 23:02:01 -0800

    Add project files.

---
+++ GA_FieldsProperties_Hongfei/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldsProperties_Hongfei
+{
+    internal class Program
+    {
+        //Hongfei
+        //week2-Fields and Properties
+        //2024-1-13
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Week2 Assignment Fields and Properties");
+
+            Student student1 = new Student();
+            Student student2 = new Student();
+            student1.Name = "Sam Lee";
+            student1.Id = 30;
+            student2.Id = 150;
+            Console.WriteLine("The student name is " + (student1.Name) + " and ID is "+ (student1.Id));
+            Console.WriteLine(student1.ReadOnlyName);
+            Console.WriteLine(student2.ReadOnlyID);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GA_FieldsProperties_Hongfei/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldsProperties_Hongfei
+{
+    public class Student
+    {
+        //Add private fields to prevent public access and change it
+        private string _name;
+        private int _id;
+
+        //Add public property to allow user return and assign value
+        public string Name
+        {
+            //Return the value from the field 
+            get { return _name; }
+            //Alow the user to assign the value to _name 
+            set 
+            {
+                if (value != "")
+                _name = value; 
+            }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if ( value >0 && value < 100)
+                _id = value; 
+            }
+        }
+
+        //Add a Read-only properties,no set accessor
+        // When user don't need check the value validation, only need get return the value of the variable
+        public string ReadOnlyName
+        { 
+            get { return _name; }
+        }
+        //Add Write-only properties no get accessor
+        //When user only need check the value if it is validation, don't need get the return value
+        public string WriteOnlyName
+        {
+            set { _name = value; }
+        }
+
+        public int ReadOnlyID
+        {
+            get { return _id; }
+        }
+
+        public int WriteOnlyID
+        {
+            set
+            {
+                if (value >0 && value < 100)
+                    _id = value;
+            }
+        }
+
+
+
+
+
+
+    }
+}
